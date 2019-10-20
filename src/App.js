1a0823b6e164d5c6debdcbb65de56659438a3f07
peter <peetumon@gmail.com>
2019-10-20 21:16:28 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// rafce
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            feeba is a good girl
+        </div>
+    )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// imr
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './App';
+//root is an Html div element in public folder index.htm file
+ReactDom.render(
+    <App />, document.getElementById('root')
+)
